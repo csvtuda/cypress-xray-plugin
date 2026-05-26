21558d8637d8db275533c82878d848cae886ae55
Sebastian Vollbrecht <sebivo@freenet.de>
2026-05-26 12:46:26 +0200

    Shard integration tests

--- test/run-integration-tests.ts
+++ test/run-integration-tests.ts
@@ -1,6 +1,6 @@
 import { createWriteStream } from "node:fs";
 import { resolve } from "node:path";
-import { run } from "node:test";
+import { run, TestShard } from "node:test";
 import { junit, spec } from "node:test/reporters";
 import { startServer, stopServer } from "./server";
 import { findFiles } from "./util";
@@ -9,6 +9,7 @@ const INTEGRATION_DIR = resolve("test", "integration");
 
 const TEST_STREAM = run({
     files: findFiles(INTEGRATION_DIR, (filepath: string) => filepath.endsWith(".spec.mts")),
+    shard: getShard(),
 })
     .once("test:fail", () => {
         process.exitCode = 1;
@@ -22,3 +23,9 @@ const TEST_STREAM = run({
 
 TEST_STREAM.compose(junit).pipe(createWriteStream("integration.xml", "utf-8"));
 TEST_STREAM.pipe(spec()).pipe(process.stdout);
+
+function getShard(): TestShard {
+    const index = Number.parseInt(process.env.INTEGRATION_TESTS_SHARD_INDEX ?? "1", 10);
+    const total = Number.parseInt(process.env.INTEGRATION_TESTS_SHARD_TOTAL ?? "1", 10);
+    return { index, total };
+}
